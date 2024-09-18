497e14328332c41b9db278609057b733c5ee5730
JayannaT <jayanna.tucker26@compscihigh.org>
2024-09-18 15:14:13 +0000

    deli order

--- more.js
+++ more.js
@@ -4,6 +4,6 @@ my room
 // Stage and commit this change with an appropriate message
 // Task 5
 // Print your go to deli order
-
+chopped cheese with everthing on it but tomato
 // Stage and commit this change with an appropriate message
 // Go to README.md
