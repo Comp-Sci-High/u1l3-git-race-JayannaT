be1dfdb89d680c517c8ee612079e56a7eeaaf7c5
JayannaT <jayanna.tucker26@compscihigh.org>
2024-09-18 15:12:53 +0000

    fave aritist

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ nutella to go bars
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
-
+cash cobain
 // Stage and commit this change with an appropriate message
 // Go to more.js
